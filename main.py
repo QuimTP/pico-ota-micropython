6d6d0dc6f269b60b82d550b06400e9eb7c0e1aa5
QuimTP <94195092+QuimTP@users.noreply.github.com>
2025-07-15 23:52:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import ugit
 from machine import Pin
 import time
 
-pin = Pin(0,Pin.IN,Pin.PULL_UP)
+pin = Pin(14,Pin.IN,Pin.PULL_UP)
 if pin.value() is 0:
-    ugit.pull_all()
-    
+    ugit.pull_all() 
+
 #main code here
-TIME_MS=100
+TIME_MS=500
 LED = Pin("LED", Pin.OUT)
 while True:
     LED.off()
